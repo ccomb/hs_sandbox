699d0fad4436e57e8e16c993308aa126f53be4d1
Christophe Combelles <ccomb@free.fr>
2016-11-20 20:32:41 +0100

    basic serve for an elm app

--- Main.hs
+++ Main.hs
@@ -2,20 +2,36 @@
 import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString.Lazy.Char8
+import qualified Data.ByteString.Lazy.Char8 as C (putStrLn)
+import Data.ByteString.Char8 (unpack)
 
 
-content :: BS.ByteString
-content = "Hello World"
+websocket :: Response
+websocket = responseLBS status200 [] "websocket"
+
+
+notFound :: Response
+notFound = responseLBS status404 [] "40404"
+
+
+serveFile :: FilePath -> Response
+serveFile filename =
+    responseFile
+    status200 
+    []
+    ("../elm/" ++ filename)
+    Nothing
+
 
 app :: Application
-app _ respond = do
-    putStrLn "Request received"
-    respond $ responseLBS status200 [] content
+app request respond =
+    respond $ case rawPathInfo request of
+        "/" -> serveFile "index.html"
+        "/ws" -> websocket
+        _ -> serveFile $ unpack $ rawPathInfo request
 
 
 main :: IO ()
 main = do
-    putStrLn $ "Listening on localhost:8080"
+    C.putStrLn "Listening on localhost:8080"
     run 8080 app
