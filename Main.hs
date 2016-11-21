3fd923e511e7324b0d87198b6a1b7c5397208c21
Christophe Combelles <ccomb@free.fr>
2016-11-21 23:39:56 +0100

    page 404 + moved assets to /static

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 import qualified Data.ByteString.Lazy.Char8 as C (putStrLn)
 import Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString as BS (take)
 
 
 websocket :: Response
@@ -11,7 +12,7 @@ websocket = responseLBS status200 [] "websocket"
 
 
 notFound :: Response
-notFound = responseLBS status404 [] "40404"
+notFound = responseLBS status404 [] "404 Not Found"
 
 
 serveFile :: FilePath -> Response
@@ -19,16 +20,20 @@ serveFile filename =
     responseFile
     status200 
     []
-    ("../elm/" ++ filename)
+    ("../elm" ++ filename)
     Nothing
 
 
 app :: Application
 app request respond =
-    respond $ case rawPathInfo request of
-        "/" -> serveFile "index.html"
+    respond $ case prefix of
+        "/" -> serveFile "/index.html"
         "/ws" -> websocket
-        _ -> serveFile $ unpack $ rawPathInfo request
+        "/static" -> serveFile $ unpack path
+        _ -> notFound
+        where
+            path = rawPathInfo request
+            prefix = BS.take 7 path
 
 
 main :: IO ()
